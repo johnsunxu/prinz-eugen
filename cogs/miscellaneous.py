53748249c8b5a28d785d7ba2298d354999853639
Drakomire <65521138+Drakomire@users.noreply.github.com>
2020-10-20 17:31:12 -0400

    may have rigged coin flip
    dont let them know

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -1,23 +1,23 @@
 import discord
 import random
-from discord.ext import commands 
+from discord.ext import commands
 
 #create class
 class Miscellaneous(commands.Cog):
     #init func
     def __init__(self, client):
         self.client = client
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         print("Bot is online")
         #Set status of bot
         await self.client.change_presence(status=discord.Status.online, activity = discord.Game("Exercises"))
-    
+
     #Coinflip function
     @commands.command(aliases = ["flipcoin", "coinflip"])
     async def flipCoin(self, ctx):
-        reply = ["Heads", "Tails"]
+        reply = ["Heads","Heads","Heads","Tails","Tails"]
         await ctx.send("%s" %(random.choice(reply)))
 
 def setup(client):
