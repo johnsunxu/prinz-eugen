d04a7b9ed5780c9a4a99f6fe0bb9e0e9bd919d02
Drakomire <65521138+Drakomire@users.noreply.github.com>
2020-10-22 15:04:39 -0400

    working guessing game

--- rushBot.py
+++ rushBot.py
@@ -1,7 +1,7 @@
 import random
 import discord
 import os
-from discord.ext import commands 
+from discord.ext import commands
 
 
 client = commands.Bot(command_prefix = ";")
@@ -13,16 +13,16 @@ async def load(ctx, extension):
     await ctx.send("Load successful!")
 
 #Unload cogs
-@client.command()  
-async def unload(ctx, extension): 
+@client.command()
+async def unload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
     await ctx.send("Unload successful!")
 
 #Scan for cogs
 for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"): 
+    if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
 
-client.run("NzU1OTc4MzQyMjg4NzE5OTgy.X2LJsg.5llVkpjjXdq2WufOHOaXCMLTdWo")
-    
+#client.run("NzU1OTc4MzQyMjg4NzE5OTgy.X2LJsg.5llVkpjjXdq2WufOHOaXCMLTdWo")
+client.run("NzMzOTkyODM2ODczMzIyNTI3.XxLOGQ.lOPYsnq0sdEIDE9EdhowJPnAg8c")
