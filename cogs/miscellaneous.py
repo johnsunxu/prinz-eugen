d929405eb5670498b93e4bbf072f7e4d81d27bf8
Drakomire <65521138+Drakomire@users.noreply.github.com>
2020-12-12 22:32:49 -0500

    Update miscellaneous.py

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -45,9 +45,9 @@ class Miscellaneous(commands.Cog):
 
     @commands.command(aliases=["patchnotes"])
     async def patchNotes(self, ctx):
-        embed = discord.Embed(title = "Patch Notes - November 25th 2020")
+        embed = discord.Embed(title = "Patch Notes - December 12th 2020")
         embed.add_field(name ="Version 1.8", value = "** **", inline = False)
-        embed.add_field(name = "** **", value = "**NEW** Added `;ehp. View ;ehp help for more info!")
+        embed.add_field(name = "** **", value = "**NEW** eHP calculator now supports PvE by using the argument PvE.")
         await ctx.send(embed = embed)
 
 def setup(client):
